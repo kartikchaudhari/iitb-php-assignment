94ab9a593be841021b89fe21fe06a45271bcfe66
Kartik <kartikchaudhari456@gmail.com>
2021-10-12 22:54:58 +0530

    Feedback Portal: Feedback Form Design completed

---
+++ student-portal/dashboard.php
@@ -0,0 +1,122 @@
+<?php 
+    session_start();
+    $_SESSION['token']=bin2hex(random_bytes(32));
+    $_SESSION['token-expire']=time()+3600;
+    include "include/functions.php";
+    require "include/Captcha.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Student Feedback Portal :: Submit Feedback</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?=base_url('assets/css/bootstrap.min.css');?>" rel="stylesheet">
+    <link href="<?=base_url('assets/css/bootstrap-theme.min.css');?>" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link rel="stylesheet" href="<?=base_url('assets/font-awesome/css/font-awesome.min.css');?>">
+</head>
+<body>
+    <!-- navbar -->
+    <?php include "nav.php";?>
+    <!--./navbar-->
+    
+    <!-- page content-->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title text-center"><i class="fa fa-plus"></i>  Submit Feedback</h3>
+                        </div>
+                        <div class="panel-body">
+                            <?php 
+                                if (isset($_SESSION['message'])) {
+                                    echo $_SESSION['message'];
+                                    unset($_SESSION['message']);
+                                }
+                            ?>
+                            <form method="post" action="<?=base_url('action.php');?>">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Name: <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control" disabled="disabled" value="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Date: <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control" disabled="disabled" value="<?=date("d/m/Y")?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Discipline: <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control" disabled="disabled" value="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Semester: <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control" disabled="disabled" value="<?=date("d/m/Y")?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="col-md-10">
+                                            <div class="form-group">
+                                                <label>Topic Attended: <span class="text-danger">*</span></label>
+                                                <input type="text" class="form-control" value="">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <div class="form-group">
+                                                <label>&nbsp;<span class="text-danger">&nbsp;</span></label>
+                                                <button type="button" class="btn btn-success"><i class="fa fa-plus"></i> Add Topic</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="row-fluid">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Your Feedback: <span class="text-danger">*</span></label>
+                                            <textarea name="" id="input" class="form-control" rows="3" required="required"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <input type="hidden" name="token" value="<?=$_SESSION["token"]?>">
+                                <center><button name="btnStudentLogin" type="submit" class="btn btn-success">Submit</button>
+                                <button type="reset" class="btn btn-danger">Reset</button></center>
+                            </form> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--./page content-->
+
+    <!-- JavaScript -->
+    <script src="<?=base_url('assets/js/jquery-1.10.2.js');?>"></script>
+    <script src="<?=base_url('assets/js/bootstrap.js');?>"></script>
+</body>
+</html>
--- student-portal/nav.php
+++ student-portal/nav.php
@@ -1,12 +1,45 @@
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">Student Feedback Portal</a>
-        </div>
+<nav class="navbar navbar-inverse" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?=base_url("#");?>">Student Feedback Portal</a>
+    </div>
+
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav">
-            <li><a href="#">Home</a></li>
+            <?php 
+                if (!isset($_SESSION['st_id'])) {
+            ?>
             <li><a href="<?=base_url('register.php');?>"><i class="fa fa-plus"></i> Registration</a></li>
             <li><a href="<?=base_url('login.php');?>"><i class="fa fa-sign-in"></i> Login</a></li>
+            <?php
+                }
+                else{
+            ?>
+            <li><a href="<?=base_url('dashboard.php');?>"><i class="fa fa-plus"></i> Submit Feedback</a></li>
+            <?php
+                }
+            ?>
         </ul>
-    </div>
+        
+        <?php 
+            if (isset($_SESSION['st_id'])) {
+        ?>
+        <ul class="nav navbar-nav navbar-right navbar-user">
+            <li class="dropdown user-dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                    <li><a href="<?=base_url('logout.php');?>"><i class="fa fa-power-off"></i> Log Out</a></li>
+                </ul>
+            </li>
+        </ul>
+        <?php
+            }
+        ?>
+
+    </div><!-- /.navbar-collapse -->
 </nav>
